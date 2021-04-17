4ec7a35345a3ad8702b9a01d23ce4759287c6290
DonDon2121 <81027157+DonDon2121@users.noreply.github.com>
2021-04-17 09:03:04 -0500

    dc

--- app/src/main/java/edu/rockvalleycollege/github7/MainActivity.kt
+++ app/src/main/java/edu/rockvalleycollege/github7/MainActivity.kt
@@ -3,7 +3,7 @@ package edu.rockvalleycollege.github7
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 //Donavan Cochran
-// Github
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
